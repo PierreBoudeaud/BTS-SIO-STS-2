904e7112e39f4b8eeccdbb124d63ac1c2eb96c5b
QUINTELA Joey <jquintela@bts.sio>
2016-11-29 09:32:43 +0100

    Création UserPannel Besoin du CSS pour aligner les éléments correctement

--- kernel/View/layout/userpanel.php
+++ kernel/View/layout/userpanel.php
@@ -1,5 +1,27 @@
 <?php
-	$userpanel="
-	<img id='imgUtilisateur' src=".IMG."default-user.png>
-	";
+	$isConnected=false;
+	
+	
+	
+	if ($isConnected){
+		$nomUtil_BDD="Laëgoline";
+		$imgUtil_BDD="gwentitia.png";
+		
+		$lienConnexion = "<a id='btn_deconnexionSmall' href='#'>Deconnexion</a>";
+	}
+	else
+	{
+		$nomUtil_BDD="Non connecté";
+		$imgUtil_BDD="default-user.png";
+		
+		$lienConnexion = "<a id='btn_connexionSmall' href='#'>Connexion</a>";
+	}
+	
+	
+	
+	$userpanel= "<img id='imgUtilisateur' src=" . IMG . $imgUtil_BDD . ">"
+					. $nomUtil_BDD . "<br/>"
+					. $lienConnexion;
+	
+	
 ?>
