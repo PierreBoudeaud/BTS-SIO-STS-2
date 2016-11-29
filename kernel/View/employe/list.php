6c911f8d630f48473c81003e59890964ea88c66a
Pierre BOUDEAUD <pierre.boudeaud@live.fr>
2016-11-29 09:10:55 +0100

    Réduction informations View employe list

--- kernel/View/employe/list.php
+++ kernel/View/employe/list.php
@@ -6,22 +6,14 @@
 				<th>Matricule</th>
 				<th>Nom</th>
 				<th>Prénom</th>
-				<th>Adresse</th>
-				<th>Ville</th>
-				<th>Code Postal</th>
-				<th>Téléphone</th>
-				<th>Portable</th>
-				<th>Email</th>
-				<th>Date Embauche</th>
-				<th>Quotité</th>
 				<th></th>
 			</tr>
 			<?php
 			foreach($this->viewvar as $employe){
-				echo "<tr>";
-				foreach($employe as $val){
-						echo "<td>{$val}</td>";
-					}
+                                echo "<tr>";
+				echo "<td>{$employe['emp_matricule']}</td>";
+                                echo "<td>{$employe['emp_nom']}</td>";
+                                echo "<td>{$employe['emp_prenom']}</td>";
 				echo "<td><a href='".WEBROOT."employe/view/".$employe['emp_matricule']."'>Afficher</a></td>";
 				echo "</tr>";
 			}
